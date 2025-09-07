ed1f8c3553e3c07d00eaf27bbfe3a37a6e4b35d7
AHartmaier <alexander.hartmaier@rub.de>
2025-09-07 16:31:59 +0200

    Added library paths to gfortran options

--- src/pylabdd/buildtools/setup_fortran.py
+++ src/pylabdd/buildtools/setup_fortran.py
@@ -45,6 +45,7 @@ class BuildFortran(_build_py):
         print(f"[BuildFortran] Using FFLAGS: {fflags}, {type(fflags)}")
         ldlibs  = os.environ.get("LDFLAGS", "").split()
         print(f"[BuildFortran] Libraries: {ldlibs}, {type(ldlibs)}")
+        fflags.append(ldlibs)
         try:
             # Let fmodpy build into its own subdirectory PK_force/
             fmodpy.fimport(
